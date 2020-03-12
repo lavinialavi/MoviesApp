35d98b60a54b3c422feb9cf522487080da7225ef
lavinia <laviniutza_90@yahoo.com>
2020-03-12 16:02:21 +0200

    "log in modifyed"

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
-// document.getElementById("usenam").addEventListener('input', sendLoginData);
-// document.getElementById("pass").addEventListener('input', sendLoginData);
+document.getElementById("usenam").addEventListener('input', prepareDateToPost);
+document.getElementById("pass").addEventListener('input', prepareDateToPost);
 
-// function prepareDateToPost() {
-// 	class User {
-// 		constructor() {
-// 			this.username =  document.getElementById("usenam").value;
-// 			this.password = document.getElementById("pass").value;
-	
+function prepareDateToPost() {
+	class User {
+		constructor() {
+			this.username =  document.getElementById("usenam").value;
+			this.password = document.getElementById("pass").value;
+		
 // //login
-// 	sendLoginData (User) {
-// 		return $.ajax("/auth/login", {
+// 	sendLoginData (User); {
+// 		return $.ajax("https://movies-api-siit.herokuapp.com/auth/login", {
 // 			method:"POST",
 // 			data: User,
 // 			success:(response) => {
@@ -19,32 +19,55 @@
 // 			error:(xhr) => {
 // 				const parsedMessage = JSON.parse(xhr.responseText);
 // 				alert('STATUS ' + xhr.status + '. ' + parsedMessage.message);
-// 			},
-// 	});
-// 	};
+// 			}
+// 	})
+// 	}
 // 		}
 // 	}
 // }
 
-const myForm= document.getElementById('myForm');
+// const myForm= document.getElementById('myForm');
 
-myForm.addEventListener('submit', function (e){
-	e.preventDefault();
+// myForm.addEventListener('submit', function (e){
+// 	e.preventDefault();
 
-	const formData = new FormData(this);
+// 	const formData = new FormData(this);
 
 
-	fetch('login.php',{
-		method: 'post',
-		body: formData
+// 	fetch('login.php',{
+// 		method: 'post',
+// 		body: formData
 
-	}).then(function (response){
-		return response.text();
-	}).then(function (text){
-	    console.log(text);
+// 	}).then(function (response){
+// 		return response.text();
+// 	}).then(function (text){
+// 	    console.log(text);
 
-	}).catch(function (error){
-		console.error(error);
-	})
+// 	}).catch(function (error){
+// 		console.error(error);
+// 	})
 
+// })
+// 		}
+// 	}
+
+
+const data = { username: 'example' };
+
+fetch('https://movies-api-siit.herokuapp.com/auth/login', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify(User),
+})
+.then((response) => response.json())
+.then((data) => {
+  console.log('Success:', User);
+})
+.catch((error) => {
+  console.error('Error:', User);
 });
+		}
+	}
+}
