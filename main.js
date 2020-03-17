6aa4c22c82c3e922036e62fca1d3b9c65219fc9a
tefiti <oanamarinica@yahoo.com>
2020-03-17 10:07:32 +0200

    delete movie

--- main.js
+++ main.js
@@ -1,26 +1,11 @@
 const apiURL = "https://movies-api-siit.herokuapp.com/movies";
-let next = null; //nextPage
-let prev = null; //previousPage
-let first = "https://movies-api-siit.herokuapp.com/movies?take=10&skip=0";
-let last = "https://movies-api-siit.herokuapp.com/movies?take=10&skip=90";
+let next = null; //next page
+let prev = null; //previous page
+let first = "https://movies-api-siit.herokuapp.com/movies?take=10&skip=0"; //firt page
+let last = "https://movies-api-siit.herokuapp.com/movies?take=10&skip=90"; //last page
 let pageNr;
 let nrOfPages;
 
-function showLoader() {
-  document.querySelectorAll(".loader, .loaderBackground").forEach(element => element.classList.add("show"));
-  // var loaderBackground = document.getElementsByClassName("loaderBackgound")[0];
-  // var loader = document.getElementsByClassName("loader")[0];
-  // loaderBackground.classList.add("show");
-  // loader.classList.add("show");
-}
-
-function hideLoader() {
-  document.querySelectorAll(".loader, .loaderBackground").forEach(element => element.classList.remove("show"));
-  // var loader = document.getElementsByClassName("loader")[0];
-  // var loaderBackground = document.querySelector(".loaderBackgound");
-  // loader.classList.remove("show");
-  // loaderBackground.classList.remove("show");
-}
 
 function getDataFromServer(param) {
   showLoader();
@@ -44,7 +29,7 @@ function removeError(){
   var errorContainer = document.getElementById("error");
   errorContainer.innerText = "";
 };
-
+let movie; 
 function displayMovie(movies) {
   pageNr = movies.pagination.currentPage;
   nrOfPages = movies.pagination.numberOfPages;
@@ -55,11 +40,12 @@ function displayMovie(movies) {
   emptyContainer(movieList);
   removeError();
   for (var i = 0; i < movies.results.length; i++) {
-    var movie = movies.results[i];
+    movie = movies.results[i];
     createMovieItem(movie);
   }
   console.log(movies);
 }
+
 document.getElementById("movieList");
 
 function createMovieItem(movie) {
@@ -67,30 +53,34 @@ function createMovieItem(movie) {
   itemContainer.classList.add("movie-item");
   if (movie.Poster != "N/A") {
     itemContainer.innerHTML = `
-    <div class="card h-100 bg-warning" style="margin:0.5rem">
+    <div class="card bg-warning" style="margin:0.5rem">
     <img class="poster card-img-top" src='${movie.Poster}' alt="Batman movie poster"/>
     <div class="card-body">
     <h3 class="movieItem-Title card-title">${movie.Title}</h3>
     <p class="movieItem-details card-text">${movie.Year}, ${movie.Genre}</p>
     <p class="movieItem-details card-text">IMDB Rating: ${movie.imdbRating}</p>
     <a href="movieDetails.html" class="stretched-link movieDetailsLink"/a>
-      </div>
+    <a href="#" class="btn btn-danger deleteButton">Delete</a>
+    </div>
     </div>`;
   } else {
     itemContainer.innerHTML = `
-  <div class="card h-100 bg-warning">
+    <div class="card h-100 bg-warning">
   <img class="poster card-img-top" src='./assets/images/marcin-lukasik-uYpOYyJdhRE-unsplash.jpg' alt="Batman"/>
   <div class="card-body">
   <h3 class="movieItem-Title card-title">${movie.Title}</h3>
   <p class="movieItem-details card-text">${movie.Year}, ${movie.Genre}</p>
   <p class="movieItem-details card-text">IMDB Rating: ${movie.imdbRating}</p>
-  <a href="movieDetails.html" class="stretched-link"/a>
-    </div>
+  <a href="movieDetails.html" class="stretched-link movieDetailsLink"/a>
+  <a href="#" class="btn btn-danger deleteButton">Delete</a>
+  </div>
   </div>`;
-  }
-  movieList.appendChild(itemContainer);
-  
-  itemContainer.addEventListener("click", () => sessionStorage.setItem("movieID", movie._id));
 }
-sessionStorage.clear();
+movieList.appendChild(itemContainer);
+itemContainer.addEventListener("click", () => sessionStorage.setItem("movieID", movie._id));
+itemContainer.addEventListener("click", deleteMovie);
+}
+
+
+
 getDataFromServer(apiURL);
